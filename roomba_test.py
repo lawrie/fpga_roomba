cae4559b54c1e08973b6af0c236670df90f4ff7c
lawrie <lawrie.griffiths@ntlword.com>
2023-02-08 17:20:50 +0000

    Added button debouncer, but sensors not working

--- roomba_test.py
+++ roomba_test.py
@@ -4,6 +4,8 @@ from amaranth.build import *
 
 from blackice_mx import *
 
+from debouncer import Debouncer
+
 roomba_pmod= [
     Resource("roomba", 0,
             Subsignal("dd",      Pins("4", dir="o", conn=("pmod",5)), Attrs(IO_STANDARD="SB_LVCMOS")))
@@ -34,7 +36,7 @@ class RoombaTest(Elaboratable):
         spot = 134
         clean = 135
         do_max = 136
-        move = 137
+        drive = 137
         motors = 138
         set_leds = 139
         song = 140
@@ -44,7 +46,7 @@ class RoombaTest(Elaboratable):
 
         # Parameters and times
         speed = 200
-        turn_time = 2.5
+        turn_time = 2.3
         forward_time = 5
         wake_time = 2
         wait_time = 1
@@ -65,17 +67,32 @@ class RoombaTest(Elaboratable):
         sensor = Signal(40)
         i = Signal(4)
 
+        # Set leds to to bump sensors
+        #m.d.comb += [
+        #    leds[0].eq(sensor[0]),
+        #    leds[1].eq(sensor[1])
+        #]
+
+        # Create debouncers for the buttons
+        m.submodules.deb1 = deb1 = Debouncer()
+        m.submodules.deb2 = deb2 = Debouncer()
+
+        m.d.comb += [
+            deb1.btn.eq(btn1),
+            deb2.btn.eq(btn2)
+        ]
+
         # Control state machine
         with m.FSM():
             with m.State("BUTTON"):
-                m.d.sync += [
-                    leds[0].eq(0),
-                    leds[1].eq(0)
-                ]
-                with m.If(btn1):
+                #m.d.sync += [
+                #    leds[0].eq(0),
+                #    leds[1].eq(0)
+                #]
+                with m.If(deb1.btn_up):
                     m.next = "BEGIN"
-                with m.If(btn2):
-                    m.next = "DOCK"
+                with m.If(deb2.btn_up):
+                    m.next = "SENSORS"
             with m.State("BEGIN"):
                 m.d.sync += [
                     serial.tx.ack.eq(0),
@@ -95,7 +112,6 @@ class RoombaTest(Elaboratable):
                         cnt.eq(0),
                         # Send start command
                         cmd[:8].eq(start),
-                        l.eq(0),
                         sending.eq(1)
                     ]
                     m.next = "START"
@@ -104,41 +120,46 @@ class RoombaTest(Elaboratable):
                     m.d.sync += [
                         # Send control command
                         cmd[:8].eq(control),
-                        l.eq(0),
                         sending.eq(1)
                     ]
                     m.next = "CONTROL"
             with m.State("CONTROL"):
+                with m.If(~sending):
+                    m.d.sync += [
+                        # Send full command
+                        cmd[:8].eq(full),
+                        sending.eq(1)
+                    ]
+                    m.next = "FULL"
+            with m.State("FULL"):
                 m.d.sync += cnt.eq(cnt + 1)
                 with m.If(cnt == (clk_freq * wait_time)):
                     m.d.sync += [
                         cnt.eq(0),
-                        # Send move command
-                        cmd[:8].eq(move),
+                        # Send drive command
+                        cmd[:8].eq(drive),
                         cmd[8:16].eq(0),
                         cmd[16:24].eq(speed),
                         cmd[24:32].eq(0x80),
                         cmd[32:].eq(0),
-                        l.eq(4),
                         sending.eq(1)
                     ]
                     m.next = "FORWARD"
             with m.State("FORWARD"):
                 m.d.sync += [
                     cnt.eq(cnt + 1),
-                    leds[0].eq(1)
+                    #leds[0].eq(1)
                 ]
                 # Wait 5 seconds
                 with m.If(cnt == (clk_freq * forward_time)):
                     m.d.sync += [
                         # Send spinleft
                         cnt.eq(0),
-                        cmd[:8].eq(move),
+                        cmd[:8].eq(drive),
                         cmd[8:16].eq(0),
                         cmd[16:24].eq(speed),
                         cmd[24:32].eq(0),
                         cmd[32:].eq(1),
-                        l.eq(4),
                         sending.eq(1)
                     ]
                     m.next = "SPINLEFT"
@@ -149,31 +170,29 @@ class RoombaTest(Elaboratable):
                     m.d.sync += [
                         # Send forward
                         cnt.eq(0),
-                        cmd[:8].eq(move),
+                        cmd[:8].eq(drive),
                         cmd[8:16].eq(0),
                         cmd[16:24].eq(speed),
                         cmd[24:32].eq(0x80),
                         cmd[32:].eq(1),
-                        l.eq(4),
                         sending.eq(1)
                     ]
                     m.next = "FORWARD2"
             with m.State("FORWARD2"):
                 m.d.sync += [
                     cnt.eq(cnt + 1),
-                    leds[1].eq(1)
+                    #leds[1].eq(1)
                 ]
                 # Wait 5 seconds
                 with m.If(cnt == (clk_freq * 5)):
                     m.d.sync += [
                         # Send spinleft
                         cnt.eq(0),
-                        cmd[:8].eq(move),
+                        cmd[:8].eq(drive),
                         cmd[8:16].eq(0),
                         cmd[16:24].eq(speed),
                         cmd[24:32].eq(0),
                         cmd[32:].eq(1),
-                        l.eq(4),
                         sending.eq(1)
                     ]
                     m.next = "SPINLEFT2"
@@ -184,12 +203,11 @@ class RoombaTest(Elaboratable):
                     m.d.sync += [
                         # Send stop
                         cnt.eq(0),
-                        cmd[:8].eq(move),
+                        cmd[:8].eq(drive),
                         cmd[8:16].eq(0),
                         cmd[16:24].eq(0),
                         cmd[24:32].eq(0x80),
                         cmd[32:].eq(1),
-                        l.eq(4),
                         sending.eq(1)
                     ]
                     m.next = "STOP"
@@ -199,12 +217,23 @@ class RoombaTest(Elaboratable):
             with m.State("DOCK"):
                 m.d.sync += [
                     # Send dock
-                    cnt.eq(0),
                     cmd[:8].eq(dock),
-                    l.eq(0),
                     sending.eq(1)
                 ]
                 m.next = "BUTTON"
+            with m.State("SENSORS"):
+                m.d.sync += [
+                    cmd[:8].eq(read_sensors),
+                    cmd[8:16].eq(1), # read 10 bytes
+                    sending.eq(1),
+                    leds[0].eq(1)
+                ]
+                m.next = "SENSOR_WAIT"   
+            with m.State("SENSOR_WAIT"):
+                m.d.sync += cnt.eq(cnt + 1)
+                with m.If(cnt == int(clk_freq * 0.5)):
+                    m.d.sync += cnt.eq(0)
+                    m.next = "BUTTON"
 
         # Send command state machine
         with m.FSM():
@@ -216,9 +245,14 @@ class RoombaTest(Elaboratable):
                         serial.tx.data.eq(cmd[:8]),
                         serial.tx.ack.eq(1)
                     ]
-                    # Move command takes 4 bytes of parameters
-                    with m.If(cmd[:8] == 137):
+                    # Drive command takes 4 bytes of parameters
+                    with m.If(cmd[:8] == drive):
                         m.d.sync += l.eq(4)
+                    with m.Elif((cmd[:8] == read_sensors) | (cmd[:8] == play) |
+                                (cmd[:8] == set_baud) | (cmd[:8] == motors)):
+                        m.d.sync += l.eq(1)
+                    with m.Elif(cmd[:8] == set_leds):
+                        m.d.sync += l.eq(3)
                     m.next = "SEND"
             with m.State("SEND"):
                 m.d.sync += serial.tx.ack.eq(0)
@@ -237,6 +271,7 @@ class RoombaTest(Elaboratable):
 
         # Read sensor data
         with m.If(serial.rx.rdy):
+            m.d.sync += leds[1].eq(1)
             m.d.sync += sensor.word_select(i, 8).eq(serial.rx.data)
             with m.If(i == 9):
                 m.d.sync += i.eq(0)
